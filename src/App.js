4eb6c25520850c6fda1052366c99e16ebc635078
Puja Fitzpatrick <pujafitz@gmail.com>
2019-11-20 09:44:27 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Activity from './components/Activity';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className='App'>
+      <Activity />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Activity.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Activity = () => {
+  return (
+    <div>
+      <h1>Itty Bitty's Activity 🐱</h1>
+    </div>
+  );
+};
+
+export default Activity;
