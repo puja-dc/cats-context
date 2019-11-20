75d9fa097f6ffb7982771c8b341b966d4ecc7c03
Puja Fitzpatrick <pujafitz@gmail.com>
2019-11-20 10:45:15 -0500

    Added 3 buttons, handleClick

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 import Activity from './components/Activity';
 
@@ -7,12 +7,24 @@ import { StateProvider } from './context';
 function App() {
   const initialState = {
     name: 'Itty Bitty',
-    activity: 'being cute'
+    activity: 'eating'
+  };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'changeActivity':
+        return {
+          ...state,
+          activity: action.activity
+        };
+      default:
+        return state;
+    }
   };
 
   return (
     <div className='App'>
-      <StateProvider value={initialState}>
+      <StateProvider value={useReducer(reducer, initialState)}>
         <Activity />
       </StateProvider>
     </div>
--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import StateContext from '../context';
 
 const Activity = () => {
+  const [value, dispatch] = useContext(StateContext);
+
+  const handleClick = e => {
+    e.preventDefault();
+    dispatch({
+      type: 'changeActivity',
+      activity: e.target.value
+    });
+  };
+
   return (
     <div>
-      <h1>Itty Bitty's Activity 🐱</h1>
+      <h1>
+        Itty Bitty's Activity{' '}
+        <span role='img' aria-label='cat'>
+          🐱
+        </span>
+      </h1>
+      <p>
+        {value.name} is {value.activity}.
+      </p>
+      <ul>
+        <li>
+          <button className='btn' onClick={handleClick} value='napping'>
+            Napping
+          </button>
+        </li>
+        <li>
+          <button className='btn' onClick={handleClick} value='eating'>
+            Eating
+          </button>
+        </li>
+        <li>
+          <button className='btn' onClick={handleClick} value='playing'>
+            Playing
+          </button>
+        </li>
+      </ul>
     </div>
   );
 };
