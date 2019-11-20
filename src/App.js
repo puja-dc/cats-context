f2ede67ef4c75d3d171bc009e1cbb9af3d43a0b9
Puja Fitzpatrick <pujafitz@gmail.com>
2019-11-20 09:50:54 -0500

    Added styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import './App.css';
 import Activity from './components/Activity';
 
+import { StateProvider } from './context';
+
 function App() {
+  const initialState = {
+    name: 'Itty Bitty',
+    activity: 'being cute'
+  };
+
   return (
     <div className='App'>
-      <Activity />
+      <StateProvider value={initialState}>
+        <Activity />
+      </StateProvider>
     </div>
   );
 }
